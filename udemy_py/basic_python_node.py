e844cd0bccff402799b53652fbf8030bc7225a00
Kalash <kalashjain513@gmail.com>
2023-01-07 21:23:04 +0530

    converting basic node code into OOP style code

--- udemy_py/basic_python_node.py
+++ udemy_py/basic_python_node.py
@@ -3,10 +3,21 @@
 import rclpy
 from rclpy.node import Node
 
+class BasicNode(Node):
+    
+    def __init__(self):
+        self.counter = 0
+        super().__init__("basic_python")
+        self.get_logger().info("Hello World")
+        self.create_timer(0.5, self.timer_callback)
+        
+    def timer_callback(self):
+        self.counter += 1
+        self.get_logger().info(f"Hello {self.counter}")
+        
 def main(args = None):
     rclpy.init(args=args)
-    node = Node("basic_python")
-    node.get_logger().info("Hello World")
+    node = BasicNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
