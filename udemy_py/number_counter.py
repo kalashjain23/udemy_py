cdb08a40528d75a2cf1ff69d600fa9f4d54e64c5
Kalash <kalashjain513@gmail.com>
2023-01-12 23:03:20 +0530

    Activity 002: number_counter now publishes and subscribes simultaneously

--- udemy_py/number_counter.py
+++ udemy_py/number_counter.py
@@ -3,18 +3,24 @@
 import rclpy
 from rclpy.node import Node
 
-from example_interfaces.msg import Int64
+from example_interfaces.msg import Int64, String
 
 class NumberCounter(Node):
     
     def __init__(self):
         super().__init__("number_counter")
+        self.offset = 2
         
         self.subscriber_ = self.create_subscription(Int64, "number", self.getNumber, 10)
+        self.publisher_ = self.create_publisher(String, "number_count", 10)
         self.get_logger().info("Number counter has been started!")
         
     def getNumber(self, msg):
-        self.get_logger().info(f"{msg.data}")        
+        message_ = String()
+        message_.data = f"{msg.data + self.offset}"
+        self.offset += 2 
+        
+        self.publisher_.publish(message_)     
 
 def main(args=None):
     rclpy.init(args=args)
