58b909bfd7eb08e5eac4a52ff5b5765eefd3b362
Kalash <kalashjain513@gmail.com>
2023-01-16 23:23:20 +0530

    Activity 003: Created a server which will reset the counter

--- udemy_py/number_counter.py
+++ udemy_py/number_counter.py
@@ -3,7 +3,8 @@
 import rclpy
 from rclpy.node import Node
 
-from example_interfaces.msg import Int64
+from example_interfaces.msg import Int64, String
+from std_srvs.srv import SetBool
 
 class NumberCounter(Node):
     
@@ -11,6 +12,7 @@ class NumberCounter(Node):
         super().__init__("number_counter")
         self.counter_ = 0
         
+        self.server_ = self.create_service(SetBool, "reset_number_count", self.callback_reset_counter)
         self.subscriber_ = self.create_subscription(Int64, "number", self.getNumber, 10)
         self.publisher_ = self.create_publisher(Int64, "number_count", 10)
         self.get_logger().info("Number counter has been started!")
@@ -21,6 +23,16 @@ class NumberCounter(Node):
         new_msg.data = self.counter_
         
         self.publisher_.publish(new_msg)
+    
+    def callback_reset_counter(self, request, response):
+        if(request.data):
+            self.counter_ = 0
+            response.success = True
+            response.message = "Counter is back to 0, baby!"
+        else:
+            response.success = False
+            response.message = "No changes done on the counter."
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
