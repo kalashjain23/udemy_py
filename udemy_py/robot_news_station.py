62c94a90cff3f21cf43e66e8ad7789d2519d7432
Kalash <kalashjain513@gmail.com>
2023-01-23 23:46:00 +0530

    Activity 005: Declared parameters for the robot name

--- udemy_py/robot_news_station.py
+++ udemy_py/robot_news_station.py
@@ -9,14 +9,16 @@ class RobotNewsStationNode(Node):
     
     def __init__(self):
         super().__init__("robot_news_station")
+        self.declare_parameter("robot_name", "robot")
         
+        self.robot_name_ = self.get_parameter("robot_name").value
         self.publisher_ = self.create_publisher(String, "robot_news", 10)
         self.timer_ = self.create_timer(0.5, self.publish_news)
         self.get_logger().info("Robot News Station has been started!")
         
     def publish_news(self):
         msg = String()
-        msg.data = "Beep Boop! I'm on the robot news station!"
+        msg.data = f"Beep Boop! I'm {self.robot_name_} from the robot news station!"
         
         self.publisher_.publish(msg)
 
